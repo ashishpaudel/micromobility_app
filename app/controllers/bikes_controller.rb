d17ff25a830f740f2c67121ee9a5b768924910b1
gurzu <gurzu@gurzus-MacBook-Pro.local>
2023-02-21 00:12:13 +0545

    add dashboard page for app

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,4 +1,9 @@
 class BikesController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+        @bikes = params[:search].present? ? Bike.where(['lower(location) LIKE ?', "%#{params[:search]}%".downcase]) : nil
+    end
   
     def new 
         @bike = Bike.new
